56f22461226f0442fa2928df5fa4a3b112038843
Felipe Soares Figueiredo <felipe.figueiredo@biomanguinhos.br>
2016-08-23 11:59:00 -0300

    OS specific font removed from main init

--- init.el
+++ init.el
@@ -72,7 +72,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 120 :width normal)))))
+ )
 (setq visible-bell t)
 ;(add-hook 'text-mode-hook 'turn-on-auto-fill)
 
