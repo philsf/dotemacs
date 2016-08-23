a924a60a7091154de9e1c06f7024a84a870c8443
Felipe Soares Figueiredo <felipe.figueiredo@biomanguinhos.br>
2016-08-23 11:50:39 -0300

    themes for OS specific font
    For now, the only specific configuration is the font. All other options
    are the same for all OSes.

--- philsf-ubuntu-theme.el
+++ philsf-ubuntu-theme.el
@@ -3,14 +3,7 @@
 
 (custom-theme-set-variables
  'philsf-ubuntu
- '(case-fold-search t)
- '(current-language-environment "UTF-8")
- '(default-input-method "rfc1345")
- '(inhibit-startup-screen t)
- '(display-time-mode t)
- '(global-font-lock-mode t)
- '(mouse-wheel-mode t)
- '(show-paren-mode t))
+ )
 
 (custom-theme-set-faces
  'philsf-ubuntu
