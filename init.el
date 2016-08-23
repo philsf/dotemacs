d8dd4b130b760ff77c3c4353af579aee96d961fb
Felipe Soares Figueiredo <felipe.figueiredo@biomanguinhos.br>
2016-08-23 09:35:17 -0300

    disable HTTPS in MELPA
    Windows has a bug in TLS (sic), so we must use HTTP for now.

--- init.el
+++ init.el
@@ -312,7 +312,7 @@ space does not end a sentence, so don't break a line there."
 (require 'package) ;; You might already have this line
 (add-to-list 'package-archives
 ;             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-             '("melpa" . "https://melpa.org/packages/"))
+             '("melpa" . "http://melpa.org/packages/"))
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
