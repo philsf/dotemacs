e1fa0591d225ab9e24e71ecf6162d70e9c9e7133
Felipe Figueiredo <philsf79@gmail.com>
2018-07-28 21:16:20 -0300

    use UTF-8

--- init.el
+++ init.el
@@ -10,6 +10,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(current-language-environment "UTF-8")
  '(custom-enabled-themes (quote (zerodark)))
  '(inhibit-startup-screen t)
  '(package-archive-priorities (quote (("melpa-stable" . 1) ("gnu" . 2))))
