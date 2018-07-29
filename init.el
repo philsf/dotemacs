a146b47866e485a2365e19e14905ffd95671cbcd
Felipe Figueiredo <philsf79@gmail.com>
2018-07-28 21:18:26 -0300

    magithub

--- init.el
+++ init.el
@@ -38,3 +38,9 @@
 
 ;; Magit status (2015-09-22)
 (global-set-key (kbd "C-x g") 'magit-status)
+
+;; magithub (2018-07-27)
+(use-package magithub
+  :after magit
+  :ensure t
+  :config (magithub-feature-autoinject t))
